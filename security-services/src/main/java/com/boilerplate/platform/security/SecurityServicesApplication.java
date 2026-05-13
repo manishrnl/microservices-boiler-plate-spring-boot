592f8da27771e5309ebe53b76a6afbc747a783fa
MANISH <manishrajrnl@zohomail.in>
2026-05-14 01:13:54 +0530

    Initial

--- security-services/src/main/java/com/boilerplate/platform/security/SecurityServicesApplication.java
+++ security-services/src/main/java/com/boilerplate/platform/security/SecurityServicesApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecurityServicesApplication {
+public class
+SecurityServicesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SecurityServicesApplication.class, args);
